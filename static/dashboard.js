d0c8599d50b31d3a554ae373345cd28a7ea5ddf5
psanker <psanker@splunk.com>
2015-07-29 14:13:20 -0700

    add phone glyph, don't show content initially

--- static/dashboard.js
+++ static/dashboard.js
@@ -6,11 +6,23 @@
     	$interpolateProvider.endSymbol('}]');
 	});
 	app.controller("JobsController", function() {
-		this.currentJob = 0;
+		this.currentJob = null;
 		this.jobs = jobs;
 		this.setCurrentJob = function(index) {
 			this.currentJob = index; 
-		};		
+		};
+		this.getCurrentJob = function() {
+			for(var i = 0; i < jobs.length; i++) {
+				if (jobs[i].id === this.currentJob) {
+					return jobs[i];
+				}
+			}
+		}	
+	});
+
+	app.controller("ContentController", function() {
+
+
 	});
 
 
@@ -20,34 +32,39 @@
 			'requester': 'Prakash Sanker',
 			'description': 'I want you to give me the entire world',
 			'phoneNumber': '18008080',
-			'jobStatus': 'new'
+			'jobStatus': 'new',
+			'notes': ''
 		},
 		{
 			'id': 2,
 			'requester': 'MJ Watson',
 			'description': 'Please give me Spiderman, preferably upside down...but Im not fussy',
 			'phoneNumber': '650848405',
-			'jobStatus': 'new'
+			'jobStatus': 'new',
+			'notes': ''
 		},
 		{	'id': 3,
 			'requester': 'Josh Lyman',
 			'description': 'Matthew Santos as president...or Jed Bartlet..but please not anyone Republican',
 			'phoneNumber': '1234456',
-			'jobStatus': 'progress'
+			'jobStatus': 'progress',
+			'notes': ''
 		}, 
 		{
 			'id': 4,
 			'requester': 'Barry Allen',
 			'description': 'A frictionless suit that wont burst into flames when I run at a zillion miles an hour...its for a friend',
 			'phoneNumber': '6508045509',
-			'jobStatus': 'progress'
+			'jobStatus': 'progress',
+			'notes': ''
 		},
 		{
 			'id': 5,
 			'requester': 'A. Bambaata',
 			'description': 'Boombox, prime condition, from the 80s. Go back in time if you have to',
 			'phoneNumber': '6508045509',
-			'jobStatus': 'resolved'
+			'jobStatus': 'resolved',
+			'notes': ''
 		}
 	];
 
