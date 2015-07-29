6dded73142dd4feaf858bef388c1c102345b4d26
psanker <psanker@splunk.com>
2015-07-29 14:46:21 -0700

    adding extra contacts

--- static/dashboard.js
+++ static/dashboard.js
@@ -25,15 +25,24 @@
 
 	});
 
+	app.controller("NotesController", function() {
+
+
+	});
+
 
 	var jobs = [
 		{
 			'id': 1,
 			'requester': 'Prakash Sanker',
-			'description': 'I want you to give me the entire world',
+			'description': 'I want you to give me the entire world. Jk. The new Lebron10s will do',
 			'phoneNumber': '18008080',
 			'jobStatus': 'new',
-			'notes': ''
+			'notes': "We haven't started looking at this case yet",
+			'contacts': [{
+				'Nike': '(415) 544-9918',
+				'Mr. Jacob Mueller': { 'phone': '(415) 5509-432', 'email': 'jmueller@nike.com','helpful':'Contact between 7pm and 10pm on Thursdays - he will pick up more often'
+			}]
 		},
 		{
 			'id': 2,
@@ -41,7 +50,11 @@
 			'description': 'Please give me Spiderman, preferably upside down...but Im not fussy',
 			'phoneNumber': '650848405',
 			'jobStatus': 'new',
-			'notes': ''
+			'notes': "We haven't started this case yet - not as important as the others",
+			'contacts': [{
+				"Marvel": '(852) 123-4568',
+				"Peter Parker": {'phone': '(514) 445-8199', 'email': 'pparker@marvel.com', 'helpful': "It's better to contact him on this number - (650)804-5509"}
+			}]
 		},
 		{	'id': 3,
 			'requester': 'Josh Lyman',
